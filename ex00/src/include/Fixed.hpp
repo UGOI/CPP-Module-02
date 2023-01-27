c7b88ae624296afe3f220b957297aa8e5ec82833
Stefan Dukic <stefan@Stefans-MacBook-Pro.local>
2023-01-27 14:55:30 +0100

    add ex00

---
+++ ex00/src/include/Fixed.hpp
@@ -0,0 +1,13 @@
+class Fixed
+{
+private:
+	int                 _fixedPointValue;
+	static const int    _fractionalBits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed &copy);
+	~Fixed();
+	Fixed &operator=(const Fixed &copy);
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
